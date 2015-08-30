b5a44a1213e5de4f8d6dfcead34d5d12dcd60db3
SnowShock35 <SnowShock35@users.noreply.github.com>
2015-08-30 15:56:03 +0100

    Update ModProps.java

--- src/main/java/jsonAbles/ModProps.java
+++ src/main/java/jsonAbles/ModProps.java
@@ -3,5 +3,5 @@ package jsonAbles;
 public class ModProps {
 	public static final String modid = "jsonables", MODID = modid;
 	public static final String name = "JSON-Ables", NAME = name;
-	public static final String version = "1.0.8", VERSION = version;
+	public static final String version = "1.0.9", VERSION = version;
 }
